65b636e34ec5f9e4494f82b6801dd3cbf622af13
DiegoArias1527 <diego.arias.torres@gmail.com>
2023-11-14 14:04:26 -0500

    Se suben archivos de la primera API

---
+++ APIs/controller/productos.php
@@ -0,0 +1,32 @@
+<?php
+    header('Content-Type: application/json');
+
+    require_once("./APIs/config/conexion.php");
+    require_once("./APIs/models/Productos.php");
+    $productos = new Productos();
+    $body = json_decode(file_get_contents("php://input"), true);
+
+    switch ($_GET["op"]) {
+        case "GetAll":
+            $datos=$productos->get_productos();
+            echo json_encode($datos);
+        break;
+
+        case "GetId":
+        $datos=$productos->get_productos_x_id($body["id_Producto"]);
+        echo json_encode($datos);
+        break;
+
+        case "Insert":
+            $datos=$productos->insert_productos(
+                $body["id_Producto"],
+                $body["Nom_Producto"],
+                $body["Precio_Producto"],
+                $body["Desc_Producto"],
+                $body["Id_Categoria"],
+                $body["Id_Proveedor"]
+            );
+            echo "Correcto";
+        break;
+    }
+?>
---
+++ APIs/models/Productos.php
@@ -0,0 +1,38 @@
+<?php
+class Productos extends Conectar{
+    public function get_productos(){
+        $conectar = parent :: conexion();
+        parent:: set_names();
+        $sql="SELECT * FROM productos";
+        $sql=$conectar->prepare($sql);
+        $sql->execute();
+        return $resultado=$sql->fetchAll();
+    }
+
+    public function get_productos_x_id($id_Producto){
+        $conectar= parent::Conexion();
+        parent::set_names();
+        $sql="SELECT * FROM productos WHERE id_Producto = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $id_Producto);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insert_productos($id_Producto,$Nom_Producto,$Precio_Producto,$Desc_Producto,$Id_Categoria,$Id_Proveedor){
+        $conectar= parent::Conexion();
+        parent::set_names();
+        $sql="INSERT INTO productos (Id_Producto,Nom_Producto,Precio_Producto,Desc_Producto,Id_Categoria,Id_Proveedor) VALUES ('?', '?', '?', '?', '?', '?');";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1, $id_Producto);
+        $sql->bindValue(2, $Nom_Producto);
+        $sql->bindValue(3, $Precio_Producto);
+        $sql->bindValue(4, $Desc_Producto);
+        $sql->bindValue(5, $Id_Categoria);
+        $sql->bindValue(6, $Id_Proveedor);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+?>
